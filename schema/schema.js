ba7c8c5e65d846e75c40bec727d1867149364901
dmitryure <dmitryure@gmail.com>
2020-04-09 16:58:51 +0300

    working with array of users

--- schema/schema.js
+++ schema/schema.js
@@ -1,15 +1,31 @@
-const graphql = require('graphql');
-const {
-    GraphQLObjectType,
-    GraphQLInt,
-    GraphQLString
-} = graphql
+const graphql = require("graphql");
+const {find} = require("lodash")
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema } = graphql;
+
+const users = [ {id: '1', firstName:'vasya', age:20}, {id: '2', firstName:'petya', age:50} ]
 
 const userType = new GraphQLObjectType({
-    name: "User",
-    fields: {
-        id: {type: GraphQLString},
-        firstName: {type: GraphQLString},
-        age: {type: GraphQLInt}
-    }
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: userType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return find(users, {id: args.id})
+      }
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
